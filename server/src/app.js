7a4157aed27376b95fcae5b201eb2269a4f940b2
Luis Guerra <luis910gg@gmail.com>
2025-09-15 11:25:02 -0400

    feat: Improve request logging with Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,12 @@ connectDB()
 
 const productRoutes = require('./router/product')
 
-app.use(morgan('dev'))
 app.use(express.json())
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan('Method: :method\nRes: :status\nPath: :url\nBody: :body\n------ (:total-time[0]ms)'))
 
 app.use(`/api`, productRoutes)
 
