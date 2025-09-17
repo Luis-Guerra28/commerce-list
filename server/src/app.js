5b85f1db144e32c5c3cff452840d71f79f7ed5e1
Luis Guerra <luis910gg@gmail.com>
2025-09-17 01:17:58 -0400

    feat: Add CORS middleware to enable cross-origin requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,8 +9,8 @@ connectDB()
 
 const productRoutes = require('./router/product')
 
+app.use(cors())
 app.use(express.json())
-
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
