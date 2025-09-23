a4f332d3a8a6f4508cbf7a932b691b253ae7200a
Luis Guerra <luis910gg@gmail.com>
2025-09-23 19:12:32 -0400

    feat: Integration of ThemeProvider and CssBaseline in principal component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import { ProductsList } from './components/Products'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-});
-
 function App() {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <h1>Hello world</h1>
-        <ProductsList />
-      </ThemeProvider>
+      <h1>Hello world</h1>
+      <ProductsList />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 import App from './App.jsx'
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
